f0836bab1968a5ecae5c07c21b95935e1a17b131
FigmaPrompt <ben.kemp.alida@gmail.com>
2025-12-24 20:47:39 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,36 @@
 def filter_even(numbers: list[int]) -> list[int]:
+    """
+    Return a list of even numbers from the input list.
+    """
+    # TODO: implement
+    pass
+
+
+
+
+
+
+def filter_even(numbers: list[int]) -> list[int]:
     return [n for n in numbers if n % 2 == 0]
 
-print(filter_even([1, 2, 3, 4, 6]))
+def test_mixed_numbers():
+    assert filter_even([1, 2, 3, 4, 6]) == [2, 4, 6]
 
+
+def test_no_even_numbers():
+    assert filter_even([1, 3, 5]) == []
+
+
+def test_empty_list():
+    assert filter_even([]) == []
+
+
+def test_includes_zero():
+    assert filter_even([0, 1, 2]) == [0, 2]
+
+
+def test_negative_numbers():
+    assert filter_even([-4, -3, -2, -1]) == [-4, -2]
+
+
+print(filter_even([1, 2, 3, 4, 6]))
